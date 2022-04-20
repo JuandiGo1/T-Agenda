90aeef9e62ce855ea53a8e3be9cc60f147a5fabf
hasuarez <98895019+hasuarez@users.noreply.github.com>
2022-04-20 10:31:49 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -25,3 +25,41 @@ class Admin(User):
         pass
     def AsignarHorario(Empleado):
         pass
+class turno:
+
+
+    turnoAsignado= 0
+    cargoAsignado = "Ninguno"
+
+    def __init__(self):
+        self.tunoAsignado= 0
+        self.cargoAsignado= "Ninguno"
+
+
+
+class Horario:
+    dias = "Ninguno"
+    horas = 0  
+
+    def__init__(self):
+        self.dias = "Ninguno"
+        self.horas = 0
+
+class posicion:
+
+    #Aqui ocuparemos la API de google maps para obtener la posicion del usuario 
+    # y guardaremos esta posicion en una variable llamada coordenadas
+    coordenadas = posision_usuario 
+
+    def compararCoordenadas():
+        #esta funcion nos va a servir para comparar la ubicacion del usuario
+        #con la ubicacion del usuario utilizando la api de google 
+
+
+class puntaje:
+
+
+    puntual = verdadero
+
+    def __init__(self):
+        self.puntual = verdadero
